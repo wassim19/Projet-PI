73916240dff41e75646e6239944aac6a94d3e274
wassim19 <47669122+wassim19@users.noreply.github.com>
2021-02-27 15:04:40 +0100

    rendezvous

---
+++ src/Controller/RendezVousController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\RendezVous;
+use App\Form\RendezVousType;
+use App\Repository\RendezVousRepository;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RendezVousController extends AbstractController
+{
+    /**
+     * @Route("/rendez/vous", name="rendez_vous")
+     */
+    public function index(): Response
+    {
+        return $this->render('rendez_vous/index.html.twig', [
+            'controller_name' => 'RendezVousController',
+        ]);
+    }
+
+    /**
+     * @param RendezVousRepository $repository
+     * @return Response
+     * @Route("/affiche",name="rendezvous")
+     */
+    public function listclassrrom(RendezVousRepository $repository){
+        $rendezvous=$repository->findAll();
+        return $this->render('rendez_vous/affiche.html.twig',array("rendezvous"=>$rendezvous));
+    }
+    /**
+     *  @Route ("/add",name="addrendez-vous")
+    */
+    function add(Request $request){
+        $rendezvous=new RendezVous();
+        $form=$this->createForm(RendezVousType::class,$rendezvous);
+        $form->add('Ajouter',SubmitType::class);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $em=$this->getDoctrine()->getManager();
+            $em->persist($rendezvous);
+            $em->flush();
+            return $this->redirectToRoute('rendezvous');
+        }
+        return $this->render("rendez_vous/index.html.twig",array('form'=>$form->createView()));
+
+    }
+    /**
+     * @Route ("/supp/{id}",name="d")
+     */
+    public function Delete( $id , RendezVousRepository $repository)
+    {
+        $rendezvous = $repository-> find($id);
+        $em=$this->getDoctrine()->getManager();
+        $em->remove($rendezvous);
+        $em->flush();
+        return $this->redirectToRoute('rendezvous');
+    }
+    /**
+     * @Route("rendezvous/Update/{id}",name="update")
+     */
+    function Update(RendezVousRepository $repository,$id,Request $request){
+        $rendezvous=$repository->find($id);
+        $form=$this->createForm(RendezVousType::class,$rendezvous);
+        $form->add('Update',SubmitType::class);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $em=$this->getDoctrine()->getManager();
+            $em->flush();
+            return $this->redirectToRoute('rendezvous');
+        }
+        return $this->render("rendez_vous/update.html.twig",array('f'=>$form->createView()));
+
+    }
+}
---
+++ src/Entity/RendezVous.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RendezVousRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=RendezVousRepository::class)
+ */
+class RendezVous
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $meet;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getMeet(): ?string
+    {
+        return $this->meet;
+    }
+
+    public function setMeet(string $meet): self
+    {
+        $this->meet = $meet;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
