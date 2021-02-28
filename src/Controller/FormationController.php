62cbe7817f77aead257e9cc09eb5ff76997f7295
fedi <fedi.benammar@esprit.tn>
2021-02-28 20:10:56 +0100

    crudformation

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Formation;
+use App\Form\FormationType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\FormationRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,4 +22,57 @@ class FormationController extends AbstractController
             'controller_name' => 'FormationController',
         ]);
     }
+
+    /**
+     * @Route("/afficheformation", name="afficheformation")
+     */
+    public function index(): Response
+    {
+
+        $rep=$this->getDoctrine()->getRepository(Formation::class);
+        $formations=$rep->findAll();
+
+        return $this->render('formation/afficheformation.html.twig', ['formations' => $formations ]);
+    }
+
+    /**
+     * @Route("/addformation", name="addformation")
+     */
+    public function addformation(Request $request)
+    {
+        $formation= new formation();
+        $form=$this->createForm(FormationType::class , $formation);
+        $form->add('Add', submittype::class);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $em= $this->getDoctrine()->getManager();
+            $em->persist($formation);
+            $em->flush();
+
+            return $this->redirectToRoute("afficheformation");
+
+        }
+
+        return $this->render('formation/addformation.html.twig', ['form' => $form->createView()]);
+    }
+
+
+    /**
+     * @Route("/delformation/{id}", name="delformation")
+     */
+    public function deleteformation(int $id): Response
+    {
+
+        $em = $this->getDoctrine()->getManager();
+        $formation = $em->getRepository(Formation::class)->find($id);
+        $em->remove($formation);
+        $em->flush();
+
+
+
+        return $this->redirectToRoute("afficheformation");
+    }
+    
 }
