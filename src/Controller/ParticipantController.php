3d27bfb64401cc37d612c7f17b4ddee047413a53
faroukgassara <46141639+faroukgassara@users.noreply.github.com>
2021-02-28 23:38:09 +0100

    farouk

---
+++ src/Controller/ParticipantController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\ParticipantE;
+use App\Form\ParticipantEType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ParticipantController extends AbstractController
+{
+    /**
+     * @Route("/participantt", name="participant")
+     */
+    public function index(): Response
+    {
+        return $this->render('participant/index.html.twig', [
+            'controller_name' => 'ParticipantController',
+        ]);
+    }
+
+    /**
+     * @Route("/participant", name="participant")
+     */
+    public function Addparticipant(Request $request)
+    {
+        $event= new ParticipantE();
+        $form=$this->createForm(ParticipantEType::class,$event);
+        $form->add('Add',SubmitType::class);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($event);
+            $entityManager->flush();
+            return $this->redirectToRoute("evenementsociete");
+        }
+
+        return $this->render('evenement_societe/participant.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Form/ParticipantEType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\ParticipantE;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ParticipantEType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('mail')
+            ->add('nom')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => ParticipantE::class,
+        ]);
+    }
+}
