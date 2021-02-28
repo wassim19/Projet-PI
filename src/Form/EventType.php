5fd7ffdbc71016a708088ae650c21203a7356bce
faroukgassara <46141639+faroukgassara@users.noreply.github.com>
2021-02-28 02:22:19 +0100

    .

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -1,1 +1,30 @@
-ph
+<?php
+
+namespace App\Form;
+
+use App\Entity\Evenement;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('dateAt')
+            ->add('title')
+            ->add('type')
+            ->add('description')
+            ->add('localitation')
+            ->add('id_societe')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Evenement::class,
+        ]);
+    }
+}
