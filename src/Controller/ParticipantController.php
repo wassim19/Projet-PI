91d14518e12bf36a81bc09c97939ecdb89dfc9d7
faroukgassara <46141639+faroukgassara@users.noreply.github.com>
2021-03-01 22:48:14 +0100

    .

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Evenement;
 use App\Entity\ParticipantE;
+use App\Entity\ParticipationE;
 use App\Form\ParticipantEType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use \Symfony\Component\HttpFoundation\Request;
@@ -27,9 +28,9 @@ class ParticipantController extends AbstractController
     }
 
     /**
-     * @Route("/participant_e", name="participant_e")
+     * @Route("/participant_e{id}", name="participant_e")
      */
-    public function index(Request $request): Response
+    public function index(Request $request,$id): Response
     {
         $event= new ParticipantE();
         $form=$this->createForm(ParticipantEType::class,$event);
@@ -37,8 +38,14 @@ class ParticipantController extends AbstractController
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid())
         {
+
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($event);
+            $entityManager->flush();
+            $participation= new ParticipationE();
+            $participation->setIdEvenement($id)
+                ->setIdParticipant($event->getId());
+            $entityManager->persist($participation);
             $entityManager->flush();
             return $this->redirectToRoute("evenementsociete");
         }
