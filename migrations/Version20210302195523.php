30e38850042c5ef3e8f3a8e3dcf328195e2bc02a
eya souissi <48189583+eyaaaaa@users.noreply.github.com>
2021-03-03 00:35:40 +0100

    ee

--- migrations/Version20210302195523.php
+++ migrations/Version20210302195523.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210228203952 extends AbstractMigration
+final class Version20210302195523 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,14 +20,12 @@ final class Version20210228203952 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE evenement ADD id_societe INT NOT NULL');
-        $this->addSql('ALTER TABLE rendez_vous CHANGE date date DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE user DROP password');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE evenement DROP id_societe');
-        $this->addSql('ALTER TABLE rendez_vous CHANGE date date DATE NOT NULL');
+        $this->addSql('ALTER TABLE user ADD password VARCHAR(240) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
     }
 }
---
+++ src/Form/CompanyType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Company;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CompanyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name_company')
+            ->add('description')
+            ->add('tax_registration_number')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Company::class,
+        ]);
+    }
+}
