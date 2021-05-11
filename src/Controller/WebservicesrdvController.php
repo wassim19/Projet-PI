714fb9fc947977f312ff70b2628d16708b17fc7d
wassim19 <47669122+wassim19@users.noreply.github.com>
2021-05-11 22:43:58 +0000

    v

--- src/Controller/WebservicesrdvController.php
+++ src/Controller/WebservicesrdvController.php
@@ -62,7 +62,7 @@ class WebservicesrdvController extends AbstractController
         $data = json_decode($content, true);
         $event->setDate(new DateTime($data['date']));
         $event->setMeet($data['meet']);
-        //$event->setMail($data['mail']);
+        $event->setMail($data['mail']);
         $event->setDescription($data['description']);
         $entityManager->persist($event);
         $entityManager->flush();
